a79ada05c3b2a73d2df8b3561e307bf94d934dda
Diego Silva <social.diegosilvatech@gmail.com>
2022-11-08 15:49:42 -0300

    chore: client side rendering

--- src/layouts/home/home.layout.tsx
+++ src/layouts/home/home.layout.tsx
@@ -1,29 +1,7 @@
-import { useState, useEffect } from 'react'; // CLIENT SIDE RENDERING
-
 import * as s from './home.layout.styles';
 
 const HomeLayout = () => {
-  // CLIENT SIDE RENDERING
-  const [repositories, setRepositories] = useState<string[]>([]);
-  useEffect(() => {
-    fetch('https://api.github.com/users/diegosilvatech/repos')
-      .then((response) => response.json())
-      .then((data) => {
-        const respositoryNames = data.map((repository) => repository.name);
-
-        setRepositories(respositoryNames);
-      });
-  }, []);
-
-  return (
-    <s.Container>
-      <ul>
-        {repositories.map((repository) => (
-          <li key={repository}>{repository}</li>
-        ))}
-      </ul>
-    </s.Container>
-  );
+  return <s.Container></s.Container>;
 };
 
 export { HomeLayout };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,28 @@
-import { HomeLayout } from 'layouts';
+// import { GetServerSideProps } from 'next';
+
+import { useState, useEffect } from 'react';
 
 export default function Home() {
-  return <HomeLayout />;
+  const [repositories, setRepositories] = useState<string[]>([]);
+  useEffect(() => {
+    fetch('https://api.github.com/users/diegosilvatech/repos')
+      .then((response) => response.json())
+      .then((data) => {
+        const respositoryNames = data.map((repository) => repository.name);
+
+        setRepositories(respositoryNames);
+      });
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {repositories.map((repository) => (
+          <li key={repository}>{repository}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+// export const getServerSideProps: GetServerSideProps = async () => {};
